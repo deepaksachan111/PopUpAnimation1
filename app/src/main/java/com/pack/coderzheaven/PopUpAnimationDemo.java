de674d199cd784c71ff03c7b122577fc68e53cbf
deepak <deepaksachan8@gmail.com>
2016-03-10 15:23:01 +0530

    Initial commit

---
+++ app/src/main/java/com/pack/coderzheaven/PopUpAnimationDemo.java
@@ -0,0 +1,61 @@
+package com.pack.coderzheaven;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class PopUpAnimationDemo extends Activity {
+
+	private Animation animShow, animHide;
+	
+	@Override
+	public void onCreate(Bundle icicle) {
+    	
+        super.onCreate(icicle);
+        setContentView(R.layout.main); 
+        initPopup();
+    }
+    
+    private void initPopup() {
+    	
+    	final SlidingPanel popup = (SlidingPanel) findViewById(R.id.popup_window);
+
+    	// Hide the popup initially.....
+    	popup.setVisibility(View.GONE);
+    	
+    	animShow = AnimationUtils.loadAnimation( this, R.anim.popup_show);
+    	animHide = AnimationUtils.loadAnimation( this, R.anim.popup_hide);
+    	
+    	final ImageButton   showButton = (ImageButton) findViewById(R.id.show_popup_button);
+    	final ImageButton   hideButton = (ImageButton) findViewById(R.id.hide_popup_button);
+    	showButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				popup.setVisibility(View.VISIBLE);
+				popup.startAnimation( animShow );
+				showButton.setEnabled(false);
+				hideButton.setEnabled(true);
+        }});
+        
+        hideButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				popup.startAnimation( animHide );
+				showButton.setEnabled(true);
+				hideButton.setEnabled(false);
+				popup.setVisibility(View.GONE);
+        }});
+
+    	final TextView locationName = (TextView) findViewById(R.id.site_name);
+        final TextView locationDescription = (TextView) findViewById(R.id.site_description);
+        
+        locationName.setText("CoderzHeaven");
+        locationDescription.setText("Heaven of all working codes"
+        							+ " A place where you can ask, share & even shout for code! Let’s share a wide range of technology here." +
+        	  						" From this site you will get a lot of working examples in your favorite programming languages!." +
+        	  						" Always remember we are only one comment away from you… Let’s shorten the distance between your doubts and your answers…");
+        							
+	}
+}
